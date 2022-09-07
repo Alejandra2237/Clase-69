a160901ebe683eb4b2834378b5376150283e918f
Alejandra2237 <alebeat-14@outlook.com>
2022-09-06 23:53:21 -0500

    update

--- App.js
+++ App.js
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
 import BottomTabNavigator from "./components/BottomTabNavigator";
+import { LogBox } from 'react-native';
 
+
+LogBox.ignoreAllLogs();
 export default class App extends Component {
   render() {
     return <BottomTabNavigator />;
